c0d73d0c1f760f4f7ab58643efefb854de0b004d
Malik Duisebayev <malik.duisebaev@mail.ru>
2022-01-19 21:22:41 +0600

    writing a slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(function(){
+    $('.slider__box').slick({
+        prevArrow: `<img class = 'slider__arrow slider__arrow-left' src="./images/arrow-left.svg" alt="left arrow">`,
+        nextArrow: `<img class = 'slider__arrow slider__arrow-right' src="./images/arrow-rigth.svg" alt="rigth arrow">`
 
+    })
 });
