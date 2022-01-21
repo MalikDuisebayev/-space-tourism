1570bdad69023ea69ad95e02e99b6b0710869853
Malik Duisebayev <malik.duisebaev@mail.ru>
2022-01-21 18:46:50 +0600

    made adaptive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 $(function(){
     $('.slider__box').slick({
         prevArrow: `<img class = 'slider__arrow slider__arrow-left' src="./images/arrow-left.svg" alt="left arrow">`,
-        nextArrow: `<img class = 'slider__arrow slider__arrow-right' src="./images/arrow-rigth.svg" alt="rigth arrow">`
+        nextArrow: `<img class = 'slider__arrow slider__arrow-right' src="./images/arrow-rigth.svg" alt="rigth arrow">`,
+        responsive: [
+            {
+              breakpoint: 1080,
+              settings: {
+                  arrows: false
+              }
+            }
+        ]
 
     })
 });
+
+const btnActive = document.querySelector('.menu__btn')
+btnActive.addEventListener('click',()=>{
+    document.querySelector('.menu__list').classList.toggle('active')
+})
